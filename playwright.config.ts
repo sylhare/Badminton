412eeab0e9af3d50e43effe898485b2a7c8eac59
sylhare <sylhare@outlook.com>
2025-09-30 17:09:07 -0400

    Fix e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [['list']] : [
     ['html', { open: 'never' }],
-    ['list']
+    ['list'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
