f66b1e907ff4d20997691fc263f426de950abeac
sylhare <sylhare@outlook.com>
2025-10-24 14:59:54 -0400

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -59,7 +59,14 @@ export default [
       'unused-imports/no-unused-imports': 'error',
       'unused-imports/no-unused-vars': [
         'warn',
-        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+        },
       ],
       'import/order': [
         'error',
